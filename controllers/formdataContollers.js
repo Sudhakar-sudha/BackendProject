048161139ecfe8b2062a08f4b6d32fde57806c0a
test <test@UGSERVER.com>
2024-12-27 15:05:46 +0530

    A

--- controllers/formdataContollers.js
+++ controllers/formdataContollers.js
@@ -1,5 +1,5 @@
 
-const FormDataModel = require('../models/formdataModel');
+// const FormDataModel = require('../models/formdataModel');
 exports.formdata = async (req, res) => {
     res.send("uploading image");
   };
