a7d6cfecf811816d0a2fe777f304da4691af3241
test <test@UGSERVER.com>
2024-12-27 15:17:09 +0530

    A

--- controllers/formdataControllers.js
+++ controllers/formdataControllers.js
@@ -1,6 +1,12 @@
 
 const FormDataModel = require('../models/formdataModel');
+const formidable = require("formidable");
 exports.formdata = async (req, res) => {
+  const form =new formidable.IncomingForm();
+  form.parse(req,(err,fields,files)=>{
+    console.log(`name:${fields.name}`);
+    console.log(`age:${fields.age}`);
+  })
     res.send("uploading image");
   };
   
