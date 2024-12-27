16adfdffad5aa6262f36625879e0485c19d556af
test <test@UGSERVER.com>
2024-12-27 15:59:37 +0530

    A

--- models/formdataModel.js
+++ models/formdataModel.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
+// Define the schema
 const formdataSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true }
+  name: { type: String, required: true }, // Name as a required string
+  age: { type: Number, required: true }, // Age as a required number
+  picture: { type: String, required: true } // Picture filename or URL as a required string
 });
 
+// Create the model
 const FormDataModel = mongoose.model('FormData', formdataSchema);
+
 module.exports = FormDataModel;
