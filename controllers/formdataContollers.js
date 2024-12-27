b7e233e8b35cbe4f4157e634a6de845d45984b9a
test <test@UGSERVER.com>
2024-12-27 15:01:58 +0530

    A

--- controllers/formdataContollers.js
+++ controllers/formdataContollers.js
@@ -1,6 +1,5 @@
 
 const FormDataModel = require('../models/formdataModel');
-
 exports.formdata = async (req, res) => {
     res.send("uploading image");
   };
--- routes/FormData.js
+++ routes/FormData.js
@@ -1,10 +1,7 @@
 const express = require("express");
-
 const { formdata } = require("../controllers/formdataControllers");
 
-
 const router = express.Router();
-
 router.post("/form", formdata);
 
 module.exports = router;
